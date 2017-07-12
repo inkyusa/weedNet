eab0628c9113cb36f31e81b9701b1763cc1bb197
enddl22 <enddl22@gmail.com>
2017-07-12 11:01:07 +0200

    Display NDVI image for better annotation.

--- MATLAB/Tools/annotate.m
+++ MATLAB/Tools/annotate.m
@@ -5,12 +5,18 @@ clc;clear; close all;
 %segpath = '/home/enddl22/extDisk/inkyu_storage/dataset/weednet/Weed-Plant-2m-all/plant/seg/';
 %imgpath = '/home/enddl22/extDisk/inkyu_storage/dataset/weednet/Weed-Plant-2m-all/plant/img/';
 
-segpath = '/home/jason/GPU/zetao_workspace/Projects/Inkyu_weednet/plant/seg';
-imgpath = '/home/jason/GPU/zetao_workspace/Projects/Inkyu_weednet/plant/img';
+segpath = '/Volumes/MyStorage/Research/weedNet/data/Sequoia/20170616-2m-plant-n-crop/Crop-Only-2m/plant/seg';
+imgpath = '/Volumes/MyStorage/Research/weedNet/data/Sequoia/20170616-2m-plant-n-crop/Crop-Only-2m/plant/img';
+ndvipath = '/Volumes/MyStorage/Research/weedNet/data/Sequoia/20170616-2m-plant-n-crop/Crop-Only-2m/ndvi/';
 
-
+allndvi = dir(fullfile(ndvipath,'*.png'));
 allimg = dir(fullfile(imgpath,'*.png'));
 allseg = dir(fullfile(segpath,'*.png'));
+
+cmap=importdata('colorMap.txt');
+cmap=cmap.data(:,2:4)/255;
+
+
 draw_last = 0;
 redraw = 0;
 idx = 1;
@@ -20,10 +26,12 @@ allmask = {};
 %for idx = 1:length(allimg)
 while (idx <= length(allimg))
     imgname = fullfile(imgpath,allimg(idx).name);
+    ndviname = fullfile(ndvipath,allndvi(idx).name);
     segname = fullfile(segpath,allseg(idx).name);
     
     imgfile = imread(imgname);
     segfile = imread(segname);
+    ndvifile = imread(ndviname);
     
     [height,width] = size(imgfile);
     tempmask = zeros(height,width);
@@ -32,7 +40,7 @@ while (idx <= length(allimg))
     imshow(imgfile);
     title('image');
     set(h1,'Units','normalized');
-    movegui(h1,[1800,0]);
+    movegui(h1,[1400,0]);
     hold on
     
     h2 = figure(2);
@@ -40,6 +48,19 @@ while (idx <= length(allimg))
     title('segmentation result');
     button_last=uicontrol('Parent',h2,'Style','pushbutton','String','draw last region','Units','normalized','Position',[0.0 0 0.2 0.1],'Visible','on','Callback','draw_last = 1;');
     button_redraw = uicontrol('Parent',h2,'Style','pushbutton','String','re-draw regions','Units','normalized','Position',[0.5 0 0.2 0.1],'Visible','on','Callback','redraw = 1;');
+    
+    h3 = figure(3);
+    ax=axes;
+    imagesc(double(ndvifile)/255);
+    title('NDVI image');
+    set(h3,'Units','normalized');
+    colormap(cmap);
+    set(ax,'xtick',[]);
+    set(ax,'ytick',[]);
+    axis image;
+    colorbar;
+    truesize;
+    movegui(h3,[2400,0]);
     
     while(1)
         
